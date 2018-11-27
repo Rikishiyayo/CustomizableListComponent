44957b397cd1c8523d089f288dd52e189a4ffab5
Cabala <jaroslav.cabala@solarwinds.com>
2018-11-27 01:38:59 +0100

    created test service and model. List component created

---
+++ src/app/models/player.ts
@@ -0,0 +1,12 @@
+export class Player {
+    public firstName: string;
+    public lastName: string;
+    public age: number;
+    public position: string;
+    public nationality: string;
+    public club: string;
+    public height: number;
+    public weight: number;
+    public contractUntil: number;
+    public marketValue: number;
+}
